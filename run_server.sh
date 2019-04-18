ae0a236d6966864644a0447898dfe342a69d3992
Alex <yrwang501@163.com>
2019-04-18 11:17:40 +0800

    Add files via upload

---
+++ run_server.sh
@@ -0,0 +1,13 @@
+$SPARK_HOME/bin/spark-submit --master "spark://wyr-HP-EliteDesk-800-G1-TWR:7077" \
+    --conf 'spark.driver.extraJavaOptions=-Dbigdl.engineType=mkldnn'\
+    --conf 'spark.executor.extraJavaOptions=-Dbigdl.engineType=mkldnn' \
+    --driver-memory 2g --class com.intel.aimaster.PredictorServer \
+	--total-executor-cores 4\
+	--executor-cores 4\
+    target/AI-Master-0.1.0-SNAPSHOT-jar-with-dependencies.jar \
+	core-site.xml \
+	hbase-site.xml \
+	hdfs://ai-master-bigdl-0.sh.intel.com:8020/model_new_helper_API_10.obj \
+	kfb_512_100_test \
+	http://localhost:13345/updateStatus
+
